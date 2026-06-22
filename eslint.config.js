db843c5aa1e597e2f9694c3a97c46d414417e7d0
Lurieee <sarmientojohnllorie@gmail.com>
2026-06-22 17:12:13 +0800

    fix: node version problem

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,39 @@
+// import globals from 'globals';
+// import eslintConfigPrettier from 'eslint-config-prettier';
+
+// export default [
+//   {
+//     languageOptions: {
+//       ecmaVersion: 2022,
+//       sourceType: 'module',
+//       globals: {
+//         ...globals.node,
+//         ...globals.jest
+//       }
+//     },
+//     rules: {
+//       'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+//       'no-console': 'off',
+//       'prefer-const': 'error',
+//       'no-var': 'error',
+//       'eqeqeq': ['error', 'always'],
+//       'curly': ['error', 'all'],
+//       'no-throw-literal': 'error',
+//       'no-return-await': 'error',
+//       'require-await': 'error'
+//     }
+//   },
+//   eslintConfigPrettier
+// ];
+
+
 import globals from 'globals';
 import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default [
   {
+    // Fix 1: Explicitly specify which files to run linting on
+    files: ['src/**/*.js', 'tests/**/*.js'],
     languageOptions: {
       ecmaVersion: 2022,
       sourceType: 'module',
@@ -18,8 +49,11 @@ export default [
       'no-var': 'error',
       'eqeqeq': ['error', 'always'],
       'curly': ['error', 'all'],
-      'no-throw-literal': 'error',
-      'no-return-await': 'error',
+      
+      // Fix 2: Replaced deprecated 'no-throw-literal' with ESLint v9 equivalent
+      'only-throw-error': 'error', 
+      
+      // Fix 3: Removed completely deprecated 'no-return-await' (no longer needed in native Node)
       'require-await': 'error'
     }
   },
